322b003e32fd9c549ff331726b6058dc5de30a88
Chocodu31 <raphaelgoul@outlook.com>
2024-02-28 14:19:20 +0100

    I done the Error Function, it print an error message via error code send when u call the function.
    (I'm gonna do test function next (i think))

--- Atelier-2-DUNE.py
+++ Atelier-2-DUNE.py
@@ -1,20 +1,22 @@
 ####################################################################
 # Avant de commencer a lire le code, il y a quelques précision que #
 # je voudrais faire :                                              #
-# - Les print("####################################") permettent   #
-#   d'afficher un message d'erreur                                 #
+# - La fonction show_error permet d'afficher un message d'erreur   #
+#   (j'ai crée cette fonction dans le but d'avoir un code plus     #
+#   clair).                                                        #
 # -
 ####################################################################
 
 
-###########################
-# Print le plateau de jeu #
-###########################
+#######################################################################
+# show_board permet d'afficher le plateau de jeu dans le format voulu #
+#######################################################################
 
 def show_board(grille):
     chiffre_show = 1
     lettre_show = ord("A")
-    # Print les lettre en haut du plateau#
+
+    # Print les lettre en haut du plateau #
     print("  ", end=" ▌ ")
     for k in range(0, len(grille[0])):
         if k == len(grille[0]) - 1:
@@ -24,7 +26,7 @@ def show_board(grille):
         lettre_show += 1
     print("")
 
-    # Print le plateau entier (en ajoutant les numéro de case au début #
+    # Print le plateau entier (en ajoutant les numéro de case au début) #
     print("    ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁")
     for y in range(0, len(grille)):
         if chiffre_show >= 10:
@@ -52,14 +54,10 @@ def show_board(grille):
 
 def input_convertor(grille, player_turn):
     player_input = input("Sélectionner le pion (Format Type : 'A01' (LettreChiffre)) : ")
-    print("")
 
     # On appelle les autre fonction pour savoir si l'input est correct #
     while not est_au_bon_format(player_input, grille, player_turn):
-
-        # Si l'input n'est pas correct, on affiche un Message d'Erreur #
-        print("Votre input est incorrect !")
-        print("####################################", end="\n\n")
+        # Si l'input n'est pas correct, on redemande un autre input #
         player_input = input("Sélectionner le pion (Format Type : 'A01' (LettreChiffre)) : ")
         print("")
 
@@ -77,7 +75,6 @@ def input_convertor(grille, player_turn):
 ############################################################################
 
 def est_au_bon_format(player_input, grille, player_turn):
-
     # Vérifie si l'input fait bien 3 caractères #
     if len(player_input) == 3:
 
@@ -93,15 +90,19 @@ def est_au_bon_format(player_input, grille, player_turn):
                 if grille[temp_ligne][temp_colonne] == player_turn:
                     # Si tout est bon, return True #
                     return True
+
                 else:
-                    # Si l'input n'est pas un pion ou n'appartient pas au joueur, on affiche un Message d'Erreur #
-                    print("####################################")
-                    print("Erreur : Ceci n'est pas votre pion ! (Ou ceci n'est pas un pion)")
+
+                    # Si l'input n'est pas un pion ou n'appartient pas au joueur, show_error affiche une erreur #
+                    # Erreur : Input03 #
+                    show_error("input03")
+
+        else:
+            # Si le format est incorrect, show_error affiche une erreur #
+            # Erreur : Input01 #
+            show_error("input01")
     else:
-        # Si le format est incorrect, on affiche un Message d'Erreur #
-        print("####################################")
-        print("Erreur : Mauvais Format !")
-        # Ce message apparait aussi lorsque le pion sélectionnée n'apartient pas au joueur #
+        show_error("input01")
 
     # Si il y a une erreur, return False #
     return False
@@ -113,13 +114,11 @@ def est_au_bon_format(player_input, grille, player_turn):
 ########################################################################
 
 def est_dans_grille(ligne, colonne, grille):
-
     # Vérifie si l'input ne dépasse pas la grille #
     if len(grille) <= ligne or ligne < 0 or colonne < 0 or len(grille[0]) <= colonne:
-
-        # Si l'input dépasse la grille, on affiche un Message d'Erreur #
-        print("####################################")
-        print("Erreur : Out of range ! (Votre input ne correspond pas a une case du plateau !)")
+        # Si l'input dépasse la grille, show_error affiche une erreur #
+        # Erreur = Input02 #
+        show_error("input02")
         # Si il y a une erreur, return False #
         return False
 
@@ -129,6 +128,35 @@ def est_dans_grille(ligne, colonne, grille):
 
 # FIN PARTIE : TRAITEMENT DE L'INPUT                                                                           #
 ################################################################################################################
+
+
+#####################################################################
+# show_error permet de print l'erreur dans un format compréhensible #
+# pour le joueur. Cela permet d'avoir un code plus propre           #
+#####################################################################
+
+def show_error(error):
+    print("")
+    print("####################################")
+
+    # Les erreur de type Input #
+    if len(error) >= 5 and error[0:5] == "input":
+
+        # L'erreur input01 == L'input est dans un mauvais format #
+        if error == "input01":
+            print("Erreur : Mauvais Format !")
+
+        # L'erreur input02 == L'input désigne une case n'existant pas sur le plateau #
+        if error == "input02":
+            print("Erreur : Out of range ! (Votre input ne correspond pas a une case du plateau !)")
+
+        # L'erreur input03 == Le pion sélectionné n'appartient pas au joueur (ou il n'y a pas de pion) #
+        if error == "input03":
+            print("Erreur : Ceci n'est pas votre pion ! (Ou ceci n'est pas un pion)")
+
+        print("Votre input est incorrect !")
+        print("####################################", end="\n\n")
+        return None
 
 
 # Le plateau #
@@ -147,6 +175,8 @@ grille_start = [
 
 show_board(grille_start)
 player_turn = "●"
+
+test = "INPUT1"
 
 print(f"Tour des pions {player_turn}")
 input_convertor(grille_start, player_turn)
