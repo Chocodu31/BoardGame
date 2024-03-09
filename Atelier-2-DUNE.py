da3142fe574b3d3e3da0be84b6766110adf77f30
Chocodu31 <raphaelgoul@outlook.com>
2024-03-09 19:18:29 +0100

    I dunno wat ive added, but its cool i swear

--- Atelier-2-DUNE.py
+++ Atelier-2-DUNE.py
@@ -1,36 +1,65 @@
 # -*- coding: utf-8 -*-
 
 ####################################################################
-# Avant de commencer a lire le code, il y a quelques précision que #
-# je voudrais faire :                                              #
+# Avant de commencer a lire le code, voici quelque informations    #
+# important pour lire et comprendre le code :                      #
 #                                                                  #
-# - L'import os permet d'utiliser la commande                      #
-#   os.system("cls;clear") est uniquement dédié à                  #
-#   clear la console.                                              #
+# - "import os" permet d'utiliser la commande                      #
+#   os.system("cls;clear") elle est uniquement dédié à             #
+#   clear la console (c'est à dire la vider totalement             #
+#   comme une nouvelle page).                                      #
 #   Cela permet d'avoir un résultat plus propre lors de            #
 #   l'exécution.                                                   #
+#   (Elle n'est pas était interdite d'utilisation, j'ai vérifié    #
+#   au près du professeur présentant le cours amphi de l'UE).      #
 #                                                                  #
-# - La fonction show_error permet d'afficher un message d'erreur   #
+# - La fonction "show_error" permet d'afficher un message d'erreur #
 #   (j'ai crée cette fonction dans le but d'avoir un code plus     #
 #   clair).                                                        #
+#   Elle n'était pas demandé cependant j'ai trouvé intéréssant     #
+#   d'en faire une (pour être plus compréhensible).                #
 #                                                                  #
+# - ".isdigit" et ".isalpha" sont des outils de python             #
+#   permettant de savoir si un caractère est un chiffre,           #
+#   ou une lettre. (Elle n'ont pas était interdite d'utilisation,  #
+#   j'ai vérifié au près du professeur présentant le cours amphi   #
+#   de l'UE).                                                      #
+#                                                                  #
+# - La fonction "debuter_partie" permet de lancer une partie       #
+#   en définisant via choix, les paramètres de celle-ci.           #
+#   Elle n'était pas demandé cependant j'ai trouvé intéréssant     #
+#   d'en faire une (pour être plus compréhensible et pratique).    #
+#                                                                  #
+# - L'ensemble des fonctions font moins de 15 lignes comme         #
+#   stipulé (Si on retire tout les commentaire et retour à la      #
+#   ligne que j'ai mit pour la lisibilité),                        #
+#   (Hors "afficher_grille", celle-ci avait le droit d'être        #
+#   plus longue).                                                  #
+#                                                                  #
+# - Puisque jeu de cet atelier ressemble fortement a un plateau    #
+#   de jeu de dame/échec, je me suis donc caler sur les même       #
+#   notation, c'est a dire la notation suivante :                  #
+#    - Les Lettre représentes des colones (de gauche à droite)     #
+#    - Les Chiffres représentes des lignes (de haut en bas)        #
+#   Il n'y avait pas d'obligation pour ce choix (seulement des     #
+#   conseils).                                                     #
 ####################################################################
 
 import os
 
 
-#######################################################################
-# show_board permet d'afficher le plateau de jeu dans le format voulu #
-#######################################################################
+############################################################################
+# afficher_grille permet d'afficher le plateau de jeu dans le format voulu #
+############################################################################
 
-def show_board(grille):
+def afficher_grille(grille):
     chiffre_show = 1
     lettre_show = ord("A")
 
     # Print les lettre en haut du plateau #
     print("  ", end=" ▌ ")
-    for k in range(0, len(grille[0])):
-        if k == len(grille[0]) - 1:
+    for colonne1 in range(0, len(grille[0])):
+        if colonne1 == len(grille[0]) - 1:
             print(chr(lettre_show), end=" ▐ ")
         else:
             print(chr(lettre_show), end=" | ")
@@ -40,16 +69,16 @@ def show_board(grille):
     # Print le plateau entier (en ajoutant les numéro de case au début) #
     # print("    ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁")
     print("    ───────────────────────────────────────")
-    for y in range(0, len(grille)):
+    for ligne in range(0, len(grille)):
         if chiffre_show >= 10:
             print(chiffre_show, "▌ ", end="")
         else:
             print(f"0{chiffre_show}", "▌ ", end="")
-        for x in range(0, len(grille[0])):
-            if x == len(grille[0]) - 1:
-                print(grille[y][x], "▐ ", end="")
+        for colonne2 in range(0, len(grille[0])):
+            if colonne2 == len(grille[0]) - 1:
+                print(grille[ligne][colonne2], "▐ ", end="")
             else:
-                print(grille[y][x], "| ", end="")
+                print(grille[ligne][colonne2], "| ", end="")
         print("")
         chiffre_show += 1
     print("    ───────────────────────────────────────")
@@ -155,7 +184,7 @@ def show_error(error):
     # Les erreur de type Input #
     if len(error) >= 5 and error[0:5] == "input":
 
-        # L'erreur input01 == L'input est dans un mauvais format #
+        # L'erreur input01 == L'input est dans un mauvais format (ou votre choix n'existe pas) #
         if error == "input01":
             print("Erreur : Mauvais Format !")
 
@@ -169,14 +198,27 @@ def show_error(error):
 
         print("Votre input est incorrect !")
         print("####################################", end="\n\n")
-    show_board(grille_start)
     return None
+
+
+def debuter_partie():
+    test_valide = False
+    while not test_valide:
+        configuration = input("Veulliez sélectionner la configuration de jeu, dans laquelle vous voulez jouer :\n 1. "
+                              "Début de partie\n 2. Milieu de partie\n 3. Fin de partie\nVotre choix : ")
+        for i in range(1, 3):
+            if configuration == str(i):
+                test_valide = True
+    # faut faire une vérification ici mais j'ai la flemme (surtout j'arrive pas)
+    tour_joueur = input("Veulliez sélectionner qui est la première personne a jouer :\n 1. ●"
+                        "\n 2. ○\nVotre choix : ")
 
 
 ################################################################################################################
 # DEBUT DE PARTIE SUR LES CONFIGURATIONS POSSIBLES                                                             #
 
 # Le plateau au début de la partie #
+
 grille_start = [
     ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
     ["○", "○", "○", "○", "○", "○", "○", "○", "○", "○"],
@@ -192,10 +234,37 @@ grille_start = [
 
 # Le plateau en milieu de partie #
 
+grille_mid = [
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["○", "○", "○", "○", "○", "○", "○", "○", "○", "○"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["●", "●", "●", "●", "●", "●", "●", "●", "●", "●"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["○", "○", "○", "○", "○", "○", "○", "○", "○", "○"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["●", "●", "●", "●", "●", "●", "●", "●", "●", "●"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
+]
 # Le plateau en fin de partie #
 
-print("Si l'affichage est trop petit, Ctrl+Molette pour zoomer (ou l'outil loupe windows)", end="\n\n")
-show_board(grille_start)
+grille_fin = [
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "●", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "●", "-", "●", "-", "-", "-"],
+    ["-", "-", "-", "-", "●", "○", "-", "●", "-", "-"],
+    ["-", "●", "-", "-", "-", "-", "●", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
+]
+
+debuter_partie()
+print("Lorsque vous exécuter le fichier .py dans l'invite de commande\nsi l'affichage est trop petit, utiliser"
+      "Ctrl+Molette pour zoomer (ou l'outil loupe windows)", end="\n\n")
+afficher_grille(grille_start)
 player_turn = "●"
 print(f"Tour des pions {player_turn}")
 input_convertor(grille_start, player_turn)
